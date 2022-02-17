7bdd79cc230bbbadf26996fd7512123b2c3adec6
Emanuele Parlascino <emapage95@gmail.com>
2022-02-17 12:39:44 +0000

    made calculator componenent and started form input structure

---
+++ pages/Calculator.js
@@ -0,0 +1,23 @@
+function Calculator(props){
+    var data = props.data;
+    var costs = props.costs;
+    var totalCost = 0;
+
+    
+
+    for (var i= 0; i<=data.length -1; i++){
+        var currentValue = 0;
+
+        currentValue = data[i] * costs[i]
+
+        totalCost += currentValue
+    }
+    
+    return(
+        <div>
+            <h2>Total: {totalCost}</h2>
+        </div>
+    )
+}
+
+export default Calculator
